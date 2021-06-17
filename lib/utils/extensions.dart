f70efa5c5d8de77d6ea489c0001a5800b990bd1c
Константин Клищук <kostyak13k@gmail.com>
2021-06-17 11:17:50 +0300

    refactored extensions

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -1,8 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_strings.dart';
 
 extension Localization on BuildContext {
   AppLocalizations get strings {
     return AppLocalizations.of(this)!;
+  }
+
+  ThemeData get theme {
+    return Theme.of(this);
   }
 }
